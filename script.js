a606e00ba432df42db536d4c18232ed1ae8bba9f
shahzad akram <alyakram786@gmail.com>
2024-02-29 16:49:47 +0500

    working on seconds

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const secondHand =  document.querySelector('.second-hand');
 const ThirdHand = document.querySelector(".min-hand")
+const FirstHand = document.querySelector(".hour-hand")
 
 function setDate(){
    
@@ -11,12 +12,22 @@ function setDate(){
     secondHand.style.transform =  `rotate(${secondsDegrees}deg)`
 
 
+if(secondHand.style.transform == `rotate(444deg)`)
+{
+    secondHand.style.transform = `rotate(90deg)`
+}
 
 
 const mins = now.getMinutes();
 const minsDegrees = ((mins / 60)* 360)+90;
-ThirdHand.style.transform = `rotate(${minsDegrees}deg)`
-}
+ThirdHand.style.transform = `rotate(${minsDegrees}deg)`;
 
+
+
+const hours = now.getHours();
+const hoursDegrees = ((hours / 12)* 360)+90;
+FirstHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+setDate()
 
 setInterval(setDate, 1000);
